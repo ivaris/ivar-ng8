914a088b0b625631a3ed21609972ff697de4cb89
Ravi Vavilipalli <rvavi@xRK-MBAir.local>
2020-07-14 18:47:24 -0700

    init changes

---
+++ src/app/data.service.ts
@@ -0,0 +1,36 @@
+import { retry, catchError, tap } from 'rxjs/operators';
+
+public first: string = "";  
+public prev: string = "";  
+public next: string = "";  
+public last: string = "";
+
+parseLinkHeader(header) {
+    if (header.length == 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    this.first  = links["first"];
+    this.last   = links["last"];
+    this.prev   = links["prev"];
+    this.next   = links["next"]; 
+  }
+
+  public sendGetRequest(){
+    // Add safe, URL encoded _page and _limit parameters 
+
+    return this.httpClient.get(this.SERVER_URL, {  params: new HttpParams({fromString: "_page=1&_limit=20"}), observe: "response"}).pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+    }));
+  }
